0d111d9a55ac977f35cffcc73b9729adad5233a1
ismetpe <ismetpe@maestralsolutions.com>
2021-10-29 13:06:09 +0200

    fluent validation added to configuration

---
+++ Core/Validators/MediaValidator.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Core.Models.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Validators
+{
+    public class MediaValidator : AbstractValidator<AddMovieDto>
+    {
+        public MediaValidator()
+        {
+            RuleFor(p => p.Title).NotEmpty();
+            RuleFor(p => p.Description).NotEmpty();
+            RuleFor(p => p.Release_year).NotEmpty();
+            RuleFor(p => p.img_url).NotEmpty();
+        }
+    }
+}
---
+++ Core/Validators/ScreeningValidator.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Core.Models.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Validators
+{
+    public class ScreeningValidator : AbstractValidator<AddScreeningDto>
+    {
+
+        public ScreeningValidator()
+        {
+            RuleFor(p => p.Time).NotEmpty();
+            RuleFor(p => p.Number_of_seats).NotEmpty().NotNull();
+            RuleFor(p => p.Number_of_tickets).NotEmpty().NotNull();
+            RuleFor(p => p.Place).NotEmpty().NotNull();
+            RuleFor(p => p.Date).NotEmpty().NotNull();
+        }
+    }
+}
